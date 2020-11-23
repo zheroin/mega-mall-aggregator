49f6bff3ac174f081be27f807c96d6f8ae24236e
CleanCodeCreature <lambertlee410@gmail.com>
2020-11-23 11:47:31 +0100

    Empty state is done (#15)
    Co-authored-by: GitGirlCommit <gicevafrose@gmail.com>

--- src/components/empty-state/empty-state.styles.tsx
+++ src/components/empty-state/empty-state.styles.tsx
@@ -2,30 +2,28 @@ import styled from 'styled-components';
 import { Box } from '@material-ui/core';
 
 const StyledDiv = styled.div`
-  position: absolute;
   width: 100%;
-  max-width: 500px;
-  height: 150px;
-  top: 50%;
-  left: 50%;
-  margin-left: -50px; /* margin is -0.5 * dimension */
-  margin-top: -25px;
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
-  text-align: center;
+  justify-content: center;
 `;
 const StyledBox = styled(Box)`
   background-color: #fff;
   height: 100%;
   min-height: 100vh;
   color: #355c7c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const StyledSvg = styled.svg`
   width: 60px;
   margin: 0 10px 0 10px;
   height: 60px;
-  display: inline-block;
+  ${props => props.theme.breakpoints.down('sm')} {
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 export { StyledDiv, StyledBox, StyledSvg };
